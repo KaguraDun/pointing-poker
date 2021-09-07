38fed7d76b3c0e3a67060ae348ad16e8c0fcf351
angie <natalia@angie.local>
2021-09-07 11:48:04 +0300

    feat: member card component

---
+++ src/components/MemberCard/MemberCard.tsx
@@ -0,0 +1,19 @@
+import 'memberCard.scss';
+
+import React from 'react';
+
+import { Member } from '../../member/interface';
+
+const MemberCard = ({ image, name, surname, position }: Member) => (
+  <div className="member-card">
+    <img alt="avatar" src={image} />
+    <h4>
+      {name}
+      {surname}
+    </h4>
+    <p>{position}</p>
+    <div className="member-card__remove" />
+  </div>
+);
+
+export default MemberCard;

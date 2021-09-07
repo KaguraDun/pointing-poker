2b983d5597100046ef89d2389ca054cc3b8d14c4
angie <natalia@angie.local>
2021-09-07 13:07:25 +0300

    feat: add styles

--- src/components/MemberCard/MemberCard.tsx
+++ src/components/MemberCard/MemberCard.tsx
@@ -1,16 +1,19 @@
-import s from 'memberCard.scss';
 import React from 'react';
 
 import { Member } from '@/models/interfaces';
 
+import s from './memberCard.scss';
+
 const MemberCard = ({ image, name, surname, position }: Member) => (
   <div className={s.memberCard}>
     <img alt="avatar" className={s.image} src={image} />
-    <h4 className={s.title}>
-      {name}
-      {surname}
-    </h4>
-    <p className={s.position}>{position}</p>
+    <div>
+      <h4 className={s.title}>
+        {name}
+        {surname}
+      </h4>
+      <p className={s.position}>{position}</p>
+    </div>
     <div className={s.remove} />
   </div>
 );

63eefd3932805bd728c4ec1e2c90369ea54014da
angietune <75065099+angietune@users.noreply.github.com>
2021-09-12 20:48:14 +0300

    fix: remove test modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import LoginForm from '@/components/LoginForm/LoginForm';
-
-import Modal from '../../components/Modal/Modal';
-
-const Home = () => {
-  const [showModal, setShowModal] = useState(false);
-  const close = () => {
-    setShowModal(false);
-  };
-  return (
-    <div>
-      Home
-      <button onClick={() => setShowModal(true)} type="button">
-        show
-      </button>
-      <Modal handleCloseModal={() => setShowModal(false)} showModal={showModal}>
-        <LoginForm handleCloseModal={close} />
-      </Modal>
-    </div>
-  );
-};
+const Home = () => <div>Home</div>;
 export default Home;

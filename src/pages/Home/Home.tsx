e4eedb27a2ad514feddaefa8c9070534d389f3e5
Vasily Kovnev <v.k95@yandex.ru>
2021-09-15 20:10:48 +0300

    feat: add create room functionality at home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Home = () => <div>Home</div>;
+import Button from '@/components/Button/Button';
+import LoginForm from '@/components/LoginForm/LoginForm';
+import Modal from '@/components/Modal/Modal';
+import { Member } from '@/models/member';
+import roomApi from '@/services/roomApi';
+
+const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory();
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const createRoom = (dealerData: Member) => {
+    roomApi.createRoom(dealerData);
+    history.push('/lobby');
+  };
+
+  return (
+    <div>
+      <Button handleClick={() => setShowModal(true)}>Create room</Button>
+      <Modal handleCloseModal={closeModal} showModal={showModal}>
+        <LoginForm
+          handleCloseModal={closeModal}
+          saveData={(dealerData) => createRoom(dealerData)}
+          userRole="dealer"
+        />
+      </Modal>
+    </div>
+  );
+};
 export default Home;

8725cc615acfde36255c4a480f206eec36a34d9a
Vasily Kovnev <v.k95@yandex.ru>
2021-09-20 20:54:52 +0300

    feat: add connect to room functionality

--- src/features/room.ts
+++ src/features/room.ts
@@ -5,14 +5,23 @@ const roomSlice = createSlice({
   name: 'room',
   initialState: {
     room: {},
+    roomNotFound: false,
+    showModalConnectRoom: false,
   },
   reducers: {
     addRoom: (state, action) => {
       state.room = action.payload;
     },
+    setRoomNotFound: (state, action) => {
+      state.roomNotFound = action.payload;
+    },
+    toggleModalConnectRoom: (state) => {
+      state.showModalConnectRoom = !state.showModalConnectRoom;
+    },
   },
 });
 
-export const { addRoom } = roomSlice.actions;
+export const { addRoom, setRoomNotFound, toggleModalConnectRoom } =
+  roomSlice.actions;
 
 export default roomSlice.reducer;
--- src/services/roomApi.ts
+++ src/services/roomApi.ts
@@ -1,18 +1,47 @@
-import { addRoom } from '@/features/room';
-import { Member } from '@/models/member';
+/* eslint-disable react-redux/useSelector-prefer-selectors */
+import {
+  addRoom,
+  setRoomNotFound,
+  toggleModalConnectRoom,
+} from '@/features/room';
+import { Member, UserEvents } from '@/models/member';
 import { Room, roomEvents } from '@/models/room';
 import store from '@/store';
 
 import socket from './SocketService';
 
 const roomApi = {
-  createRoom: (dealerData: Member) => {
-    const updatedData = { ...dealerData, ID: socket.userID };
-    socket.emit(roomEvents.CREATE_ROOM, updatedData);
-
+  createRoom(dealerData: Member) {
+    socket.emit(roomEvents.CREATE_ROOM, dealerData);
     socket.on(roomEvents.GET_ROOM_FROM_SERVER, (response: Room) => {
       store.dispatch(addRoom(response));
     });
+  },
+  getRoomFromServer(roomID: string) {
+    socket.emit(roomEvents.GET_ROOM_FROM_CLIENT, roomID);
+    socket.on(roomEvents.GET_ROOM_FROM_SERVER, (response: Room) => {
+      store.dispatch(addRoom(response));
+    });
+  },
+  connect(roomID: string) {
+    socket.emit(roomEvents.GET_ROOM_STATUS_FROM_CLIENT, roomID);
+    socket.on(roomEvents.GET_ROOM_STATUS_FROM_SERVER, (response: boolean) => {
+      if (response) {
+        socket.emit(roomEvents.CONNECT_TO_ROOM, roomID);
+        socket.on(roomEvents.GET_ROOM_FROM_SERVER, (responseRoom: Room) => {
+          store.dispatch(addRoom(responseRoom));
+        });
+        store.dispatch(toggleModalConnectRoom());
+        store.dispatch(setRoomNotFound(false));
+      } else {
+        store.dispatch(setRoomNotFound(true));
+      }
+    });
+  },
+  AddUser(userData: Member) {
+    const roomID = store.getState().room.room.ID;
+    socket.emit(UserEvents.ADD_USER_FROM_CLIENT, { userData, roomID });
+    this.getRoomFromServer(roomID);
   },
 };
 

7bfa3ddd111664b45db4d4bab41d88ebc58814d2
Vasily Kovnev <v.k95@yandex.ru>
2021-09-22 17:18:00 +0300

    feat: add lobby link

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,5 +1,40 @@
+/* eslint-disable react-redux/useSelector-prefer-selectors */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Settings = () => <div>Settings</div>;
+import Button from '@/components/Button/Button';
+import { RootState } from '@/store';
+
+import s from './Settings.scss';
+
+const Settings = () => {
+  const lobbyLink = useSelector(({ room }: RootState) => room.room.ID);
+
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(lobbyLink);
+  };
+
+  return (
+    <div>
+      <div className={s.linkWrapper}>
+        <label>
+          Link to lobby
+          <div className={s.inputWrapper}>
+            <input readOnly type="text" value={lobbyLink || ''} />
+            <Button
+              handleClick={handleCopyLink}
+              size="small"
+              variant="additional"
+            >
+              Copy
+            </Button>
+          </div>
+        </label>
+      </div>
+
+      <form>Game settings</form>
+    </div>
+  );
+};
 
 export default Settings;

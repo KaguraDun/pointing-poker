cd12fd09678f2af0bb0824bcfc4cf151c1b646e1
angie <natalia@angie.local>
2021-09-23 16:49:46 +0300

    feat: issuecard styles

--- src/components/IssueCard/IssueCard.tsx
+++ src/components/IssueCard/IssueCard.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import EditIssue from '@/icons/edit.svg';
+import DeleteIcon from '@/icons/delete.svg';
 import s from './IssueCard.scss';
 
 const IssueCard = (issueNumber: number) => (
   <div className={s.issueCard}>
-    <h4 className={s.issue}>Issue{issueNumber}</h4>
+    <div className={s.wrapper}>
+      <strong className={s.subtitle}>Current</strong>
+      <h4 className={s.title}>Issue{issueNumber}</h4>
+      <em className={s.subtitle}>Low priority</em>
+    </div>
+    <EditIssue className={s.edit} />
+    <DeleteIcon className={s.remove} />
   </div>
 );
 

5ad29e67bf74d239f17498c747d7b85e7e51cc85
Vasily Kovnev <v.k95@yandex.ru>
2021-09-29 22:02:17 +0300

    webpack: update stylelint config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const { merge } = require('webpack-merge')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 const paths = require('./paths')
 const common = require('./webpack.common.js')

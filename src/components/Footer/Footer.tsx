8b393b60d0b232ea19603f9224bbbf9edd6c56ac
angie <natalia@angie.local>
2021-09-29 20:50:35 +0300

    add: wrappers and classes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,32 +7,42 @@ import Youtube from '@/icons/youtube.svg';
 import s from './Footer.scss';
 
 const Footer = () => (
-  <div className={s.footer}>
-    <div className={s.youtube}>
-      <Youtube />
+  <footer className={s.footer}>
+    <div className={s.container}>
+      <div className={s.leftWrapper}>
+        <div className={s.youtube}>
+          <Youtube />
+        </div>
+      </div>
+      <div className={s.wrapper}>
+        <a className={s.logoRss} href="https://rs.school">
+          <LogoRss />
+        </a>
+        <div className={s.divider} />
+        <div className={s.year}>2021</div>
+      </div>
+      <div className={s.rightWrapper}>
+        <a
+          className={s.github}
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/KaguraDun"
+        >
+          <Github />
+          <span>KaguraDun</span>
+        </a>
+        <a
+          className={s.github}
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/angietune"
+        >
+          <Github />
+          <span>angietune</span>
+        </a>
+      </div>
     </div>
-    <div className={s.wrapper}>
-      <a className={s.logoRss} href="https://rs.school">
-        <LogoRss />
-      </a>
-      <div className={s.divider} />
-      <div className={s.year}>2021</div>
-    </div>
-    <div className={s.gitWrapper}>
-      <a className={s.github} href="https://github.com/KaguraDun">
-        <Github />
-        <span>KaguraDun</span>
-      </a>
-      <a className={s.github} href="https://github.com/angietune">
-        <Github />
-        <span>angietune</span>
-      </a>
-      <a className={s.github} href="https://github.com/KaguraDun">
-        <Github />
-        <span>KaguraDun</span>
-      </a>
-    </div>
-  </div>
+  </footer>
 );
 
 export default Footer;

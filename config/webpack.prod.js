a57654cd494e9686b9fea12689f42dfca7a0d507
Vasily Kovnev <v.k95@yandex.ru>
2021-09-29 22:05:04 +0300

    webpack: update production config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,14 +1,13 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const { merge } = require('webpack-merge')
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { merge } = require('webpack-merge');
 
-const paths = require('./paths')
-const common = require('./webpack.common.js')
+const paths = require('./paths');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
   output: {
     path: paths.build,
     publicPath: '/',
@@ -24,12 +23,34 @@ module.exports = merge(common, {
             loader: 'css-loader',
             options: {
               importLoaders: 2,
-              sourceMap: true,
+              sourceMap: false,
               modules: true,
             },
           },
           'postcss-loader',
-          'sass-loader',
+          {  
+          loader: 'sass-loader',
+          options: {
+            additionalData: `@import "@/styles/_variables.scss";\n@import "@/styles/_mixins.scss";`,
+          },},
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              severityError: "warning", // Ignore errors on corrupted images
+              minimizerOptions: {
+                plugins: [ 
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                ],
+              },
+            },
+          },
         ],
       },
     ],
@@ -53,4 +74,4 @@ module.exports = merge(common, {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
   },
-})
+});

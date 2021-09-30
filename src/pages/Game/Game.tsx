73d91cb247256d569fe50a6d927f710454c0148b
Vasily Kovnev <v.k95@yandex.ru>
2021-09-30 19:22:46 +0300

    feat: save/restore data on page reloading

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+/* eslint-disable react-redux/useSelector-prefer-selectors */
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-const Game = () => <div>Game</div>;
+import roomApi from '@/services/roomApi';
+import { RootState } from '@/store';
 
+import s from './Game.scss';
+
+const Game = () => {
+  const roomData = useSelector(({ room }: RootState) => room?.room);
+
+  useEffect(() => {
+    if (!Object.keys(roomData).length) {
+      roomApi.restoreDataFromServer();
+    }
+  }, [roomData]);
+
+
+  return (
+    <div className={s.game}>
+      Game
+    </div>
+  );
+};
 export default Game;

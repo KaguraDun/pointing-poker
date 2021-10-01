0af026ea238ea1486ec6a70ef932c01a95eb3202
Your Name <you@example.com>
2021-10-01 20:15:48 +0300

    fix: use UserRoles

--- src/components/MemberCard/MemberCard.tsx
+++ src/components/MemberCard/MemberCard.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Crown from '@/icons/crown.svg';
 import RemoveIcon from '@/icons/remove.svg';
-import { Member } from '@/models/member';
+import { Member, UserRoles } from '@/models/member';
 
 import s from './memberCard.scss';
 
 const MemberCard = ({ image, name, surname, position, ID, role }: Member) => (
   <li key={ID} className={s.memberCard}>
-    {role === 'dealer' ? <Crown className={s.dealer} /> : null}
+    {role === UserRoles.dealer ? <Crown className={s.dealer} /> : null}
 
     <img alt="" className={s.image} src={image.image} />
     <div>

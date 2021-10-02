aebafb2799e13ac7f43dbb4061324a6e5ba19016
Vasily Kovnev <v.k95@yandex.ru>
2021-10-02 20:21:38 +0300

    feat: add game api

--- src/services/gameApi.ts
+++ src/services/gameApi.ts
@@ -1,3 +1,4 @@
+import { Game, Round } from '@/models/game';
 import roomApi from '@/services/roomApi';
 
 const gameApi = {
@@ -9,6 +10,26 @@ const gameApi = {
   },
   setRoundTime(roundTime: number) {
     roomApi.updateGameState({ roundTime });
+  },
+  selectCard(issueID: string, cardValue: string) {
+    const userID = roomApi.getCurrentUserID();
+    this.updateRoundHistory(issueID, userID, {
+      isCardSelected: true,
+      score: cardValue,
+    });
+  },
+  updateRoundHistory(issueID: string, userID: string, RoundData: Round) {
+    const roundHistory = {
+      roundHistory: {
+        [issueID]: {
+          averageScore: null,
+          roundData: {
+            [userID]: RoundData,
+          },
+        },
+      },
+    } as any as Game;
+    roomApi.updateGameState(roundHistory);
   },
 };
 

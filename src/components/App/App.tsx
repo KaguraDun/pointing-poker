c5a1c89f4d3d3879e101329cec8561da44f1fdb2
Your Name <you@example.com>
2021-10-03 16:39:10 +0300

    fix: styles of input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
-
 import Game from '@/pages/Game/Game';
 import Home from '@/pages/Home/Home';
 import Lobby from '@/pages/Lobby/Lobby';

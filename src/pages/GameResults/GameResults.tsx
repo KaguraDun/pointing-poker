6a2b44351f21a350d7e797de22d1850770518efd
Vasily Kovnev <v.k95@yandex.ru>
2021-10-03 18:39:02 +0300

    feat: add game results page

---
+++ src/pages/GameResults/GameResults.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './GameResults.scss';
+
+function GameResults() {
+  return <div className={s.gameResults}>GAME RESULTS</div>;
+}
+
+export default GameResults;

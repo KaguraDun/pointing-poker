81dddfb8deb27daa235d492b520a362d3da500a7
Your Name <you@example.com>
2021-10-03 20:45:01 +0300

    update: styles

--- src/components/IssueCard/IssueCard.tsx
+++ src/components/IssueCard/IssueCard.tsx
@@ -10,7 +10,7 @@ const IssueCard = ({ title, link, priority }: Issue) => (
   <div key={link} className={s.issueCard}>
     <div className={s.wrapper}>
       <strong className={s.subtitle}>{link}</strong>
-      <h4 className={s.title}>Issue{title}</h4>
+      <h4 className={s.title}>Issue {title}</h4>
       <em className={s.subtitle}>{priority}</em>
     </div>
     <EditIssue className={s.edit} />
--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -105,7 +105,7 @@ const Game = () => {
         if (showUserScore) {
           return (
             <div className={s.userScore}>
-              <ScoreCard score={score || '...'} />
+              <ScoreCard score={score || '...'} key={+value.ID} />
               <div>{`${value.ID}: ${value.name} ${value.surname}`}</div>
             </div>
           );

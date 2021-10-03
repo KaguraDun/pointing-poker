3a487320f5e8b76e76cf2e40180f74fa304a8aa5
Your Name <you@example.com>
2021-10-03 15:44:51 +0300

    fix: wrong import

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Footer/header';
+import Header from '@/components/Header/Header';
 
 import Game from '@/pages/Game/Game';
 import Home from '@/pages/Home/Home';

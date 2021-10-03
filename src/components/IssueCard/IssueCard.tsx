1fe65f0d4dd750c926e7d9a704aa39b7e3d4e309
Your Name <you@example.com>
2021-10-03 12:08:41 +0300

    refactor: issuecard

--- src/components/IssueCard/IssueCard.tsx
+++ src/components/IssueCard/IssueCard.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 import DeleteIcon from '@/icons/delete.svg';
 import EditIssue from '@/icons/edit.svg';
+import { Issue } from '@/models/issue';
 
 import s from './IssueCard.scss';
 
-const IssueCard = (issueNumber: number) => (
+const IssueCard = ({ title, link, priority }: Issue) => (
   <div className={s.issueCard}>
     <div className={s.wrapper}>
-      <strong className={s.subtitle}>Current</strong>
-      <h4 className={s.title}>Issue{issueNumber}</h4>
-      <em className={s.subtitle}>Low priority</em>
+      <strong className={s.subtitle}>{link}</strong>
+      <h4 className={s.title}>Issue{title}</h4>
+      <em className={s.subtitle}>{priority}</em>
     </div>
     <EditIssue className={s.edit} />
     <DeleteIcon className={s.remove} />
